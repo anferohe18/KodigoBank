10ff02be9f5c1ba6fda58449589f330dc20900a5
Andresf <andresr427@gmail.com>
2022-04-07 20:46:02 -0500

    se configura el proyecto y se conecta a la base de datos con heroku postgress

---
+++ src/main/java/com/kodigobank/kodigobank/KodigobankApplication.java
@@ -0,0 +1,13 @@
+package com.kodigobank.kodigobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodigobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodigobankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodigobank/kodigobank/KodigobankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodigobank.kodigobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodigobankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
